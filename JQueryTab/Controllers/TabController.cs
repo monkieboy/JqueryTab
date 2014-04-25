4696c73e2e96703ad070ca78415e5f3ca263dd43
mgray <mark.gray@bglgroup.co.uk>
2014-04-25 09:30:42 +0100

    <Mark> Initial Commit

---
+++ JQueryTab/Controllers/TabController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace JQueryTab.Controllers
+{
+    public class TabController : Controller
+    {
+        //
+        // GET: /Tab/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ JQueryTab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JQueryTab.MvcApplication" Language="C#" %>
